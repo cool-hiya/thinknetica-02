24cdfee7a54ba30433cc9811f48c245b6c1757c9
Arina Busygina <thr3@mail.ru>
2020-07-26 13:55:20 +0300

    Fix tests

--- src/AuthorCard.test.js
+++ src/AuthorCard.test.js
@@ -4,14 +4,14 @@ import AuthorCard from './AuthorCard';
 import '@testing-library/jest-dom/extend-expect';
 
 test('render author card', () => {
-    author = {
+    const author = {
         name: 'Ermisenda Alvarez',
         email: 'elmiza.gmail.com',
         avatar: 'https://avatars.mds.yandex.net/get-pdb/2714303/ae71b20a-31ba-4fb9-84e1-7386270ca254/s1200?webp=false',
         description: 'Lorem ipsum lorem ipsum'
     }
 
-    const {getByText} = render(<AuthorCard author={book.author} />);
+    const {getByText} = render(<AuthorCard author={author} />);
     expect(getByText('Ermisenda Alvarez')).toBeInTheDocument;
 })
 
--- src/BookCard.test.js
+++ src/BookCard.test.js
@@ -10,12 +10,12 @@ test('render book card', () => {
         language: 'English',
         progress: 20,
         cover: 'https://i.pinimg.com/736x/40/ee/a9/40eea909c0a3b28562daf6dc50b070e8--dramas-free-books.jpg',
-        author: {
+        authors: [{
             name: 'Ermisenda Alvarez',
             email: 'elmiza.gmail.com',
             avatar: 'https://avatars.mds.yandex.net/get-pdb/2714303/ae71b20a-31ba-4fb9-84e1-7386270ca254/s1200?webp=false',
             description: 'Lorem ipsum lorem ipsum'
-        },
+        }],
         minPrice: 20,
         expectedPrice: 30,
         total: 40,
