e5d1e4954f1a8c515f00f74e9231ad307a43cf0d
Arina Busygina <thr3@mail.ru>
2020-08-29 13:44:25 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -5023,9 +5023,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Mod
   !*** ./components/modal/modal.scss ***!
   \*************************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("// extracted by mini-css-extract-plugin\nmodule.exports = {\"modal-overlay\":\"_1JM8lw4HPZeTokOUVim0xz\",\"modal-content\":\"_2Cu4AY_pi_gS9lR_PfS8PS\"};\n\n//# sourceURL=webpack:///./components/modal/modal.scss?");
+eval("throw new Error(\"Module build failed (from ../node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ../node_modules/sass-loader/dist/cjs.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\src\\\\components\\\\modal\\\\modal.scss'\\n    at C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at Array.<anonymous> (C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:203:19)\\n    at Storage.finished (C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\CachedInputFileSystem.js:55:16)\\n    at ReadFileContext.<anonymous> (C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\CachedInputFileSystem.js:91:9)\\n    at ReadFileContext.callback (C:\\\\Users\\\\Arina\\\\Desktop\\\\thinknetica-02\\\\node_modules\\\\graceful-fs\\\\graceful-fs.js:123:16)\\n    at FSReqCallback.readFileAfterOpen [as oncomplete] (fs.js:261:13)\");\n\n//# sourceURL=webpack:///./components/modal/modal.scss?");
 
 /***/ }),
 
